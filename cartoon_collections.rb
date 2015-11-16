04d8a8b7f9996eb4edc451fe51b7b95f5946578b
Justin Thomas <jthomas0209@gmail.com>
2015-11-16 16:20:55 -0500

    added methods to cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |element,index|
+    puts "#{index+1}.#{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |element|
+    element[0] = element[0].upcase
+    element << "!"
+  end
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(array)
+  value = array.find{|i| i.length > 4}
+  value == nil ? false : true
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return cheese if array.include?(cheese) == true
+  end
+  nil
 end
+  
