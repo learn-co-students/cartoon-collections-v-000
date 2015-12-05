cb26dd556a68a512599b337009c82d0a579b206c
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-05 23:37:56 +0000

    adds summon_captain_planet method

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
