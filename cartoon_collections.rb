76f55de0d941184a0b104f4004d7f7642955d8ce
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-05 23:30:40 +0000

    completes roll_call_dwarves method

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
