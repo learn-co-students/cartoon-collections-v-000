3f8c124a8fbb8027c4de5978f5713fa568a5452f
rluftw <luxh93@gmail.com>
2015-12-23 16:11:35 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(collection)
+	collection.each_with_index {|val, index| puts "#{index+1}. #{val}"} 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(collection)
+	collection.map {|val| "#{val.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(collection)
+	vals_greater_than_4 = collection.select {|val| val.size > 4}
+	vals_greater_than_4.size > 0
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(collection)
+	# the array below is here to help
+	cheese_types = ["cheddar", "gouda", "camembert"]
+
+	value = nil
+	cheese_types.each {|cheese|
+		value = cheese if collection.include?(cheese)
+		break
+	}
+	value
 end
+
