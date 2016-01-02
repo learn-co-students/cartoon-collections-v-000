3c4ad89b15143afc98884707c1c0f81e6ab3a439
fiveinfinity <jlachance1@gmail.com>
2016-01-01 21:09:41 -0600

    passed all methods using ruby array methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|i| "#{i.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  return true if array.find {|i| i.length > 4}
+  else return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.each do |i|
+    if array.include?(i) 
+      return i
+    else 
+      return nil
+    end
+  end
+
 end
