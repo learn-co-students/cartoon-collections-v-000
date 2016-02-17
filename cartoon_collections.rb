b533358ec30c04f6cadbef96737c320873aa85b2
Brian Holland <brian.holland@proatria.com>
2016-02-17 08:34:29 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  i=0
+  while i < list.length
+    puts (i+1).to_s + " " + list[i]
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(team)
+  return team.map{|member| member = member.capitalize.concat("!")}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(team)
+  return team.any?{|member| member.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.find{|item| cheese_types.include?(item)}
 end
