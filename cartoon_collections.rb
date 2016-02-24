a93e48855a4b1afdffadb68aef9cd663c8fdb06a
rafeeq-123 <rafeeq-123@hotmail.com>
2016-02-24 13:10:55 -0800

    using include, any and collect

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.collect.with_index do | index, item| puts "#{item+1}" "#{index}" 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(names)
+  names.collect do |x|
+    x = x.capitalize
+    "#{x}!"
+ end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(name)
+  name.any? do |x| x.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks)
+  if snacks.include?("cheddar")
+    return "cheddar"
+  elsif snacks.include?("gouda")
+    return "gouda"  
+  elsif snacks.include?("camembert")
+    return "camembert"
+  end
 end
