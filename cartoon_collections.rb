ab6506eafe40215d1159edb6d78e79672c4eff89
Chris Haugen <haugenc@gmail.com>
2016-03-02 05:51:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index() { |dwarf, i| puts "#{i +1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(summons)# code an argument here
   # Your code here
+  summons.collect() {|i| "#{i.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any() {|i| len(i)>4}
 end
 
 def find_the_cheese# code an argument here
