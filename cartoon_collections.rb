9bb5924aec2d9966532003086143fbbc02e0b474
SiamKing <thesiameseelectric@gmail.com>
2016-03-09 13:47:14 -0800

    who took my cheese?

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize!
+    call << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each do |type|
+    return type if cheese_types.include?(type)
+  end
+  nil
 end
