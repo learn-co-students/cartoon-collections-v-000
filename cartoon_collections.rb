c529ea319af13e9b01e3bef252a1044b7d1de4bb
808dward <808dward@gmail.com>
2016-03-16 17:08:26 -0400

    added cheeses.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+	dwarves.each.with_index(1) do |dwarf, index|
+		puts "#{index}. #{dwarf}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+def summon_captain_planet(planeteer_calls)
+	planeteer_calls.map do |call|
+	 call.capitalize + "!"
+	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+def long_planeteer_calls(long_planeteer_calls)
+	long_planeteer_calls.any? do |word|
+	 word.length > 4
+	end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(maybe_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  maybe_cheese.find do |cheese| 
+  	cheese_types.include?(cheese)
+  end
 end
