0664eeb15446f4e6edce2879678e385a2da0e846
miclp <michaelpeimani@gmail.com>
2016-03-18 10:19:34 -0700

    wrote methods to pass spec's

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, i|
+    puts "#{i + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  output = array.map {|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    return true if call.length > 4
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return cheese if snacks.include?(cheese)
+  end
+  nil
 end
