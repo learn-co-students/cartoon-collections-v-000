bdffac2f99f0385dd9c16dee3c8cb720e7132d1c
mcasciato <michael.casciato@icloud.com>
2016-03-24 14:19:40 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ def long_planeteer_calls(planeteer_calls) # code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|cheese| cheese_types.include?(cheese)}
 end
