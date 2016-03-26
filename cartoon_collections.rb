24120cfba28f8e9474abbb3d630ae8ca088d3057
Vannak Chhay <vannak.chhay1@gmail.com>
2016-03-25 23:29:01 -0400

    solve cartoon_collection methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr_of_dwarves)
+  arr_of_dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr_of_calls)
+  arr_of_calls.collect do |calls|
+    calls.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr_of_calls)
+  arr_of_calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < cheese_types.length
+    return cheese_types[i] if arr.include?(cheese_types[i])
+    i += 1
+  end
 end
