11ac818b4d3a81edf5918019931c8c7e8f93242d
nOraKat147 <n.mori.iphone@gmail.com>
2016-04-06 00:08:36 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,65 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |x, i|
+    puts "#{i+1}. " + x
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+=begin
+def summon_captain_planet(array)
+  collect = []
+  array.each do |x|
+    temp = x.capitalize
+    temp << "!"
+    collect << temp
+  end
+  puts collect.inspect
+  collect
+end
+=end
+
+def summon_captain_planet(array)
+  array.collect do |x|
+    x.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  answer = false
+  array.each do |x|
+    answer = true if x.length > 4
+  end
+  answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if (array & cheese_types).empty?
+    nil
+  else
+    (array & cheese_types).first
+  end
 end
+
+
+array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+roll_call_dwarves(array)
+
+array2 = ["doc", "dopey", "bashful", "Grumpy"]
+puts summon_captain_planet(array2).inspect
+puts array2.inspect
+
+short_words = ["puff", "go", "two"]
+puts long_planeteer_calls(short_words)
+assorted_words = ["two", "go", "industrious", "bop"]
+puts long_planeteer_calls(assorted_words)
+
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+#=> "gouda"
+ 
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
