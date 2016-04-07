5343e4498790a0bdfe42733bf5671727fe3e6eb2
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-04-07 19:55:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arg)
+  count = 1
+  arg.each do |x|
+    print "#{count}. #{x} "
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  collection = []
+  planeteer_calls.each do |x|
+    collection << x.capitalize.concat("!")
+  end
+  collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(eats)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |x|
+    if eats.include?(x)  
+      return x
+    else
+      return nil
+    end
+  end
+
+
 end
