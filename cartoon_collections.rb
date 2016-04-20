88b7c3e5d42bc4c8c315e4ecb8c9f05bb636564c
Michael Chen <michael.chen.1120@gmail.com>
2016-04-20 00:04:40 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index{|names,numbers| puts "#{numbers+1}.#{names}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map{|element| "#{element}!".capitalize }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any?{|word| word.length >4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each{|cheese| return cheese if array.include?(cheese) == true}
+  nil
 end
