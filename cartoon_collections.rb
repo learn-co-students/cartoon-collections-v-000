929ce1ffbe2241cdcbe9a79090597cc34e1aefe1
Being Young <beingy@gmail.com>
2016-05-03 19:44:01 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.collect do |call|
-    call.capitalize << "!"
-  end
+  calls.collect { |call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls(calls)
