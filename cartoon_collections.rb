67a0dd872f8300586b8f677798425602f0eb5e52
joelbitar1986 <joelbitar1986@gmail.com>
2016-05-04 17:10:12 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |name,index|
+	puts "#{index} #{name}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# def roll_call_dwarves(array)
+#	 array.each.with_index(1) { |name,index| puts "#{index} #{name}"}
+# end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |x| 
+	x.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(names)
+	names.any? do |x| 
+		x.length > 4
+	end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+# def long_planeteer_calls(names)
+#	names.any? {[x] x.length > 4}
+# end
+
+#Question: does this mean that if any of the stringers are > 4 characters the entire array will return true or just each individual index element?
+
+def find_the_cheese(array)
+	if array.include? ('cheddar' || 'gouda' || 'camembert')
+		return "cheddar"
+	else 
+		nil
+	end
 end
