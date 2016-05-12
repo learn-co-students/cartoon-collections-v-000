e4df0e685b1a05eaee6d1d57635a0e5de1b6fa9d
nadukrow <nadukrow@gmail.com>
2016-05-11 22:52:37 -0400

    adding minor changes

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,30 @@
-def roll_call_dwarves(array) #code argument goes here#
-  array.each_with_index {|name, index| puts "#{index+1} and #{name}"} #Your code here#
-end
+def roll_call_dwarves(dwarfs)
+  dwarfs= [" ", " ", " ", " ", " ", " "]
+  dwarfs.each do |dwarfs|
+  puts "1.Dopey 2.Doc 3.Bashful 4.Grumpy"
+  #puts "#{value}: #{index}"
+  #array.each do |dwarves|
+  #{}"Doc", "Dopey", "Bashful", "Grumpy"
+  end
+end 
 
-def summon_captain_planet(array) # code an argument here
-  array.collect {|call| call.capitalize << "!"} # Your code here
-end
+def summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls(array)# code an argument here
-  array.any? {|word| word.length > 4}# Your code here
-end
+  planeteer_calls.map! {|name| name.capitalize + "!"} # <--  CAPITALIZE EACH ELEMENT & ADDS '!' MARK 
 
-def find_the_cheese(array) # code an argument here
-  # the array below is here to help
+end 
+
+def long_planeteer_calls(call)
+
+call.any? {|call| call.length > 4} #RETURNS TRUE IF ANY CALLS R LONGER THAN 4 CHARACTERES
+
+end 
+
+
+
+def find_the_cheese(munchies)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|item| cheese_types.include?(item)}
-end
+  cheese_types.find {|i| munchies.include?(i)}
+
+end 
+Status API Training Shop Blog About
