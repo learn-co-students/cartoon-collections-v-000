995756e8cacdfade70d5e80eb877975f2ceef1c1
Lenn Hypolite <lenntriniman@hotmail.com>
2016-05-23 11:01:07 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index do |array, number|
+    puts "#{number+1}.*#{array}" #add one to number to start numbering from 1 instead of 0
+  end
+end
+
+def summon_captain_planet(array)
+
+end
+
+def long_planeteer_calls(array)
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
