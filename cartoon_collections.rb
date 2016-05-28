702a29253b2f8f40e59462bcd50169c2b67311ae
Joseph Lim <joseph.d.lim@gmail.com>
2016-05-27 20:31:03 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(group)
+  group.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  call = []
+  call = elements.collect { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(collection)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection.each {|object| if cheese_types.include?(object) then (return object) end}
+  nil
 end
