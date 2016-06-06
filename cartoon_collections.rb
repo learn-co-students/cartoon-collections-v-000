3a2889eb41adf1b8848bd899ef338ef455b8bfd6
jack woodward <jackwoodwardschool@gmail.com>
2016-06-06 12:21:50 -0700

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)  #puts the number (starting at 1) and name with asterisk in front of it for each array element
+    names.each_with_index do |name, index|
+        puts "#{index + 1}. *#{name}"
+    end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)     #creates a new array with map method with name of each array element capitalized and exclamation point after it.
+    planeteer_calls.map! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+    words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |type|
+    if ingredients.include?(type)
+        return type
+    else
+        return nil
+    end
+end
+
 end
