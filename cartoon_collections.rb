5cbf14b663cc80be975301a03e29238cceef2ddf
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 19:33:08 +0000

    After passing summon_captain_planet tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call[0] = call[0].capitalize
+    call += "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
