f5cbe20daa981c26b5cbbaa11f5172b8ab6a5cf8
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 19:36:19 +0000

    After passing all tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.any? {|cheese| cheese == item}
+  end
 end
