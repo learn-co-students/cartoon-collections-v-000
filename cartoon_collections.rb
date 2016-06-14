20d9bedd287e780c4ffd54c98279d51c8b32b76a
andrewmmarquis <andrewmmarquis@gmail.com>
2016-06-14 17:39:54 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,10 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-ingredients = ["crackers", "cheddar", "oyster crackers"]
-def find_the_cheese(ingredients)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.detect {|ingredient| cheese_types.any? == true}
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
