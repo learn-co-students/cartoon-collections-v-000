3fb63b91783a68b0c955e97bbcc6a245f19cc7cf
auranbuckles <auran59@gmail.com>
2016-06-19 20:54:17 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+	array.each_with_index do |dwarf, index|
+		puts "#{index + 1}. #{dwarf}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+	array.map do |planet|
+		"#{planet}".capitalize << "!"
+	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+	array.any? do |call|
+		call.length > 4
+	end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  a = array.any? do |food|
+  	if food == cheese_types[0] or food == cheese_types[1] or food == cheese_types[2]
+  		return food
+  	end
+  end
+  binding.pry
+  a ? a : nil
 end
+
+  # if array.include?(cheese_types)
+  # 	return array
+  # end
