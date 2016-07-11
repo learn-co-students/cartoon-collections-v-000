cb408a56cd743b34169386ad2114cd8266b17ae4
Lani1321 <lani.berry13@gmail.com>
2016-07-11 10:16:40 -0400

    add .each to #roll_call_dwarves, add .map to #summon_captain_planet, add .length to #long_planeteer_calls, add .include to #find_the_cheese in cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(names_array)
+  names_array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|phrase| phrase = "#{phrase.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_array)
+  calls_array.each do |call|
+    if call.length > 4
+      return true
+    end
+  end
+  false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if strings_array.include?(cheese)
+      return cheese
+    end
+  end
+nil
 end
