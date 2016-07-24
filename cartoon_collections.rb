fa686130ed790d9a6f4273a9a1c436e37f215c5b
Sammy Wu <wu.sammy@gmail.com>
2016-07-24 00:46:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+   array2 = []
+  names.each_with_index do |dwarf_name, index|
+    puts "#{index} #{dwarf_name}"# code an argument here
+    end# Your code here
+    array2 << names
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arg)
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+arg = planeteer_calls
+  #It should then capitalize each element
+  #and add an exclamation point at the end.
+  arg.map do |x|
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
