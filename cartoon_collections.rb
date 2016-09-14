b083e0b98e9bb6a1abd785eed6589e214c605c12
Aya Fushimi <aya.fushimi@gmail.com>
2016-09-14 14:34:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,21 +4,30 @@ end
 
 def summon_captain_planet(arr)
   arr.collect {|item| item.capitalize! << "!"}
-  arr
+  # arr
 end
 
+# def long_planeteer_calls(arr)
+#   tf_arr = []
+#   arr.each {|item| tf_arr << (item.length > 4)}
+#   tf_arr.include?(true)
+# end
+
 def long_planeteer_calls(arr)
-  tf_arr = []
-  arr.each {|item| tf_arr << (item.length > 4)}
-  tf_arr.include?(true)
+  arr.any? {|item| item.length > 4}
 end
+
+# def find_the_cheese(arr)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   arr.each do |item|
+#     if cheese_types.include?(item)
+#       return item
+#     end
+#   end
+#   nil
+# end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.each do |item|
-    if cheese_types.include?(item)
-      return item
-    end
-  end
-  nil
+  arr.find {|item| cheese_types.include?(item)}
 end
