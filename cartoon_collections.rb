b4cdd4112d8f5b6107f9009080364f1bd6ae1f5c
Scott Wakeman <swakeman@ca.rr.com>
2016-10-22 20:35:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  #names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  names.each_with_index do | name , number |
+    puts "#{number + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  results=veggies.map {|name| name.capitalize +"!"}
 end
 
 def long_planeteer_calls# code an argument here
