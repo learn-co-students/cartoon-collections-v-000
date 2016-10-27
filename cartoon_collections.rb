e87ccadfab22a1262f066d354448ba712a3e6053
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-27 17:57:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves l# code an argument here
+  l.each_with_index do |n,i|
+    puts "#{i+1}. #{n}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet l# code an argument here
+  l.collect do |n|
+    n[0]=(n[0].ord+'A'.ord-'a'.ord).chr
+    n+="!"
+    n
+  end
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls l# code an argument here
   # Your code here
+  l.any? do |n|
+    n.length>4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese l# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  l.find do |n|
+    cheese_types.any? do |t|
+      t==n
+    end
+  end
 end
