89befe4f64cc1b23103e2ccc3859a0870c43dedf
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-10-27 03:19:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy",]
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+     dwarves.collect {|dwarf|
+          puts "#{dwarves.index(dwarf)+1} " + dwarf
+         }
 end
-
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-end
 
-def long_planeteer_calls# code an argument here
+   planeteer_calls.collect {|planet| planet.capitalize+"!"}
+end
+short_words = ["puff", "go", "two"]
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
-end
-
-def find_the_cheese# code an argument here
+  short_words.collect {|w| w.length>4}.include?(true)
+  end
+snacks = ["crackers", "gouda", "thyme"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find{|cheese| cheese == "cheddar" ||  cheese == "gouda" ||cheese ==  "camembert"}
+
 end
