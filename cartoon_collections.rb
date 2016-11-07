2707a8e370b2ebe199c169e74da859c37b5d4a7b
Mr. Mueller <bmueller@hstat.org>
2016-11-07 14:57:15 +0000

    change argument to parameter

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves# code a parameter here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet# code a parameter here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls# code a parameter here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese# code a parameter here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
