fbad7d3ffe3bad882357d00c59e24054c84f2e60
Rachel Lim <rslim87@gmail.com>
2016-12-11 21:40:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  counter = 1
+  names.each do |name|
+    puts counter.to_s + ". " + name
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.map do |call|
+    new_array << call.capitalize + "!"
+end
+return new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |word|  word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+  cheese_types.include?(cheese)
+end
+
+  # the array below is here to help
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 end
