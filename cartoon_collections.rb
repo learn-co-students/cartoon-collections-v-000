32585e143146c0bec355f551d95d6285edb4e89a
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-16 20:36:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any?{|call| call.length >4}
 end
 
 def find_the_cheese# code an argument here
