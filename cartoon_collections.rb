9bdf448880ca9c422bbda27336df033d21893ca9
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-16 20:22:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  names = []
+  dwarves.each_with_index do |dwarf, index|
+    names.push("#{index+1}. #{dwarf}")
+  end
+  puts names
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#def summon_captain_planet(planeteer_calls)
+#  my_statements = []
+#  planeteer_calls.each do |call|
+#    my_statements << "#{call.capitalize}!"
+#  end
+#  my_statements
+# end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
