18fed3f6525564b8458e724f7fad8b93d0ebc682
Elias Ventura <eliasventura23@gmail.com>
2017-01-18 00:23:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,25 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index.collect do |name, index|
-    puts "#{index+1}. #{name}"
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
   end
-end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
-    call.capitalize + "!"
+  def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map do |call|
+      call.capitalize + "!"
+    end
   end
-end
 
-def long_planeteer_calls(long_planeteer_call)
-  long_planeteer_call.any? {|call| call.length > 4}
-end
+  def long_planeteer_calls(planeteer_calls)
+    planeteer_calls.any? {|w| w.length > 4 ? true:false}
+  end
 
-def find_the_cheese(cheese_items)
-  cheeses = ["cheddar", "gouda", "camembert"]
+  def find_the_cheese(items)
+    cheeses = ["cheddar", "gouda", "camambert"]
+    items.find do |item|
+      cheeses.include?(item)
 
-  cheese_items.find do |cheese|
-    cheeses.include?(cheese)
+
+    end
+
   end
 end
