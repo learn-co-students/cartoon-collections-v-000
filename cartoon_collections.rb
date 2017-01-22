ecbcee543c1f005579feb3cd49c2f36cd34cc7bd
Kelsey Huse <kelseyhuse30@gmail.com>
2017-01-22 20:16:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any? { |x| x.length > 4}
-    return true
-  else
-    return false
-  end
+  array.any? { |x| x.length > 4}
 end
 
 def find_the_cheese(array)
