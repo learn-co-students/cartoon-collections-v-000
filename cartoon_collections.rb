2a4580518a1491f3f660b5172987bce8175d58de
sam oteo <tortillabar@me.com>
2017-01-30 19:08:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index { |dwarf, index| puts "#{index + 1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |planeteer| planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  
+  calls.any? do |words| if words.length > 4
+    words
+  end
+  end 
+
 end
 
 def find_the_cheese# code an argument here
