354dfe6b2415eb8557ed3a09f9b9b58265326452
sam oteo <tortillabar@me.com>
2017-01-30 19:26:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,16 +11,14 @@ end
 
 
 def long_planeteer_calls(calls)# code an argument here
-  # Your code here
-  
-  calls.any? do |words| if words.length > 4
-    words
-  end
-  end 
+
+  !calls.all? { |words| words
+    if words.length <= 4}
 
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
