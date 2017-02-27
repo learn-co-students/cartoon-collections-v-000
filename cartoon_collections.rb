6046ac09ebad82943623cc5066a506f3f768a0c5
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-02-27 17:28:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,12 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect! {|planeteer| planeteer.slice(0,1).upcase + planeteer.slice(1..-1) + "!"}
+  planeteer_calls.collect! {|planeteer| planeteer.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  too_long = calls.select {|call| call.length > 4}
-  too_long == [] ? false : true
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(items)# code an argument here
