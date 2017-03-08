09741f9999a9fd4ac7fad5a0d544faa0e6669661
thesilentc <thesilentc@me.com>
2017-03-08 14:29:45 -0500

    Done.

---
+++ roll-call-colution.rb
@@ -0,0 +1,5 @@
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+end
---
+++ summon_captain_planet-solution.rb
@@ -0,0 +1,3 @@
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!" }
+end
