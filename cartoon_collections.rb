792c4b173c1bae1a106b71933bcae26e1d2d7dbe
natasha kelly <kellynatashav@yahoo.com>
2017-03-26 23:52:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array)
+  array.each_with_index {|item, value| puts "#{value+1} #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4 || word.length < 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.collect do |x|
+  return x if cheese_types.include? x
+  end
+  return #w/ the return outside of the loop it returns the final iterated value instead of an array of each iteration
 end
