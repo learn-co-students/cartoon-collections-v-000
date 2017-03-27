1f59adaaaf8b78fb8ee5865c59b4bc42f9ddd714
natasha kelly <kellynatashav@yahoo.com>
2017-03-27 01:20:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.collect do |x|
-  return x if cheese_types.include? x
-  end
-  return #w/ the return outside of the loop it returns the final iterated value instead of an array of each iteration
+  array.find {|x| cheese_types.include? x} # code block written as a simple boolean
 end
+
+#.find Passes each entry in enum to block. Returns the first for which block is not false. If no object matches, calls ifnone and returns its result when it is specified, or returns nil otherwise.If no block is given, an enumerator is returned instead.
