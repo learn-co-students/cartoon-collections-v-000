36b612779dd9a123f00fd4707fc0d9642ba9dfe3
Heather You <heatherhyou@gmail.com>
2017-04-18 00:37:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,15 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  i = 0
-  while i < array.length
-    if array[i].length > 4
-      return true
-    else
-      return false
-    end
-    i += 1
-  end
+  array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(array)
