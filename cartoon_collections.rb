677840f7b56340137082d64cbac03c5f3e81501b
Timothy Ege <retibra@gmail.com>
2017-04-28 22:37:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,12 +9,9 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.each do |element|
-    if element.length > 4
-      return true
-    end
-  end
-  return false # Your code here
+  array.any? do |element|
+      element.length > 4
+    end# Your code here
 end
 
 def find_the_cheese(array)# code an argument here
