83220c0ba2d069dfc8c087d43d208be564896421
Graham Squires <graham.squires3@gmail.com>
2017-05-09 15:48:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |names, index|
+ puts "#{index+1}#{names}"
+  end
+  # code an argument here
+#I was forgetting the iterator
+# Your code here
+
+
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#don't focus on each_with_index yet
+def summon_captain_planet(veggies)
+
+  veggies.collect do |stuff|
+    "#{stuff.capitalize}!"
+# stuff.capitalize + "!"
+    # code an argument here
+  end# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.count > 4
+    return true
+  else
+    return false
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+    types.include?(cheese_types)
+    types.each do |cheese|
+      types<<cheese
+    end
+  end
