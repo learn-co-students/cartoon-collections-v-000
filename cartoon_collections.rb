90440edc75734e6b04eb214dcb0fa2293207f514
roitblatari <roitblatari@gmail.com>
2017-07-13 17:14:13 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(names) 
+  names.each_with_index do |name, i|
+  puts "#{i + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  
+  veggies.map { |e| e.capitalize + "!" }
+end
+short_words = ["puff", "go", "two"]
+def long_planeteer_calls(short_words)
+  short_words.find do |e|
+    return true if e.size > 4
+  end
+  false
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snaks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snaks.find do |snak|
+     cheese_types.include?(snak)  
+  end
 end
