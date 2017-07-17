f0f173886517cf396a324361d9562fafab8d4533
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-17 12:04:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,31 +1,35 @@
-
-# should print  dwarf name using puts. each should be on a separate line
-# each_with_index
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array)
   array.each_with_index do |name, index|
-    puts "#{index}. #{name}"
+    puts "#{index+1}. #{name}"
   end
 end
 
-
-# It should then capitalize each element and add an exclamation point at the end. The return
-# value of this method should be an array. use .map or .collect
-def summon_captain_planet(planeteer_calls)# code an argument here
-
+def summon_captain_planet(array)
+  array.map do |call|
+    call << "!"
+    call.capitalize
+  end
 end
 
-
-# The method should tell us if any of the calls are longer than four characters.--return true or false
-def long_planeteer_calls(calls)# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |calls|
+    calls.length > 4
+  end
 end
 
-# It should then look through these strings to find and return the first string that is a type of cheese.
-# The types of cheese that appear are "cheddar", "gouda", and "camembert".If, sadly, a list of ingredients
-# does not include cheese, return nil: You can assume that all strings will be lowercase. Take a look at the .
-# include method for a hint. This method asks you to return a string value instead of printing it so keep that
-# in mind.
-def find_the_cheese(strings)# code an argument here
+# # It should then look through these strings to find and return the first string that is a type of cheese.
+# # The types of cheese that appear are "cheddar", "gouda", and "camembert".If, sadly, a list of ingredients
+# # does not include cheese, return nil: You can assume that all strings will be lowercase. Take a look at the .
+# # include method for a hint. This method asks you to return a string value instead of printing it so keep that
+# # in mind.
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.all? do |queso|
+    if array.include?(queso)
+      return queso
+    else
+      return  nil
+    end
+  end
 end
