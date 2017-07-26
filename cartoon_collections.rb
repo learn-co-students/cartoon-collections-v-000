3bac4c1540bea617559fcb6a78c260f5b780bd3c
Dominique McCloud <niquelejon@gmail.com>
2017-07-26 20:38:35 +0000

    finished cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|n| n.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+cheese.find do |n|
+  cheese_types.include?(n)
+end
 end
