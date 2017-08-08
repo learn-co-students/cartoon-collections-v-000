ef700d6c8c15f96552db54a8fd84c58b58aa1c94
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-08 23:15:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array_of_dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|planeteer| "#{planeteer.capitalize}!"}
+  planeteer_calls.collect {|planeteer| planeteer.capitalize << "!"}
 end
 
 def long_planeteer_calls(calls)
