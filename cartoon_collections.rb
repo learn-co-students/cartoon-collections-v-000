17703e712d49ce7f072ab62443d6245bb1b25321
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-02 23:20:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,52 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  new_array = []
+  array.each_with_index do |element, index|
+    location = index + 1
+    puts "#{location}. #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+ array.map! {|item| item.capitalize + "!" }
+ return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  i = 0
+  new_array = []
+  while i < calls.length
+    if calls[i].length > 4
+      new_array.push << "true"
+    else
+      new_array.push << "false"
+    end
+    i += 1
+  end
+
+  new_array.include?("true") ? true : false
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  new_array = []
+  individual_cheese = ""
+
+  while i < cheese.length
+    if cheese_types.include?(cheese[i])
+      individual_cheese = cheese[i]
+      return individual_cheese
+    else
+      individual_cheese = "false"
+    end
+    i += 1
+  end
+  if individual_cheese != "false"
+    return individual_cheese
+  else
+    nil
+  end
 end
