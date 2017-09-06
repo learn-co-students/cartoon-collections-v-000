8438c108c5bb1444bf3783eb27148dee9a35e208
aisayo <aisayo@icloud.com>
2017-09-06 00:31:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names) #passes argument of dwarf names
+  names.each_with_index do |name, index| #call on each name and index
+    puts "#{index + 1}. #{name}" #always increment by 1. use name interpolation to print names
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |name|
+    "#{name.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+    calls.any? do |x|
+      if x.length > 4
+        true
+      else
+        false
+      end
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese) #method and argument
+  cheese_types = ["cheddar", "gouda", "camembert"] #array
+  cheese.find do |i|
+    if cheese_types.include?(i)
+      return i
+    else
+    end
+    nil
+end
 end
