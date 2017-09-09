7dc8a54e7f1765bf61f11c17af81d9cc51ff3d16
Nicole Therien <nicoletherien@yahoo.com>
2017-09-09 17:54:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,15 @@ def roll_call_dwarves(dwarfs)
   end
 end
 
+#Solution answer, difference is each_with_index accepted argument
+#of 1 instead of adding to index inside the block.
+
+#def roll_call_dwarves(dwarves)
+#  dwarves.each.with_index(1) do |dwarf, index|
+#    puts "#{index}. #{dwarf}"
+#  end
+#end
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|element| element.capitalize + "!"}
 end
@@ -19,12 +28,27 @@ def long_planeteer_calls(calls)
   end
 end
 
+#Correct answer
+#def long_planeteer_calls(planeteer_calls)
+#  planeteer_calls.any? { |call| call.length > 4 }
+#end
+
+
 def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if strings.map.include?("cheddar")
-    return strings[1]
-  else
-    nil
+  strings.find do |string|
+    cheese_types.include?(string)
   end
 end
+
+#This was my original answer, knowing it was incorrect, I hard coded :(
+#def find_the_cheese(strings)
+  # the array below is here to help
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  if strings.map.include?("cheddar")
+#    return strings[1]
+#  else
+#    nil
+#  end
+#end
