779e5567d3109035d3e6cbc3783dc56674cf04f3
ff18qd <ff18qd@gmail.com>
2017-09-12 00:38:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,15 +5,24 @@ def roll_call_dwarves(names)# code an argument here
   }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect {|item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any?{|i| i.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if ingredients.include?(cheese_types[0])
+    cheese_types[0]
+  elsif ingredients.include?(cheese_types[1])
+    cheese_types[1]
+  elsif ingredients.include?(cheese_types[2])
+    cheese_types[2]
+  end
 end
