1b7a3402859266ec7ffa2eb4b92301ad4a7db5fc
ff18qd <ff18qd@gmail.com>
2017-09-12 00:44:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,7 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if ingredients.include?(cheese_types[0])
-    cheese_types[0]
-  elsif ingredients.include?(cheese_types[1])
-    cheese_types[1]
-  elsif ingredients.include?(cheese_types[2])
-    cheese_types[2]
+  ingredients.find do |ingredient|
+    cheese_types.include?(ingredient)
   end
 end
