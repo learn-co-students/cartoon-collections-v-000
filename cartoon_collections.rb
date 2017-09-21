6bf8a3630048a2683f9d121000023fcc199df52d
Emily Morgan <emorgan05@gmail.com>
2017-09-21 17:29:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    number = index + 1
+    puts "#{number}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |word|
+    if word.length > 4
+      return true
+    else
+      return false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  shared = array & cheese_types.to_a
+  return shared[0]
 end
