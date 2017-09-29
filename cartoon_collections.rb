d3892bcd99ed6456aff62a15c7a54b73d709e3ba
Rony Boeck <ramg.9@live.com>
2017-09-29 04:59:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  new_array=array.map.with_index {|x,i| (i+1).to_s + " "+ x}
+  new_array.each {|x| puts x}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i=0
+  while i<array.length
+    new_array=array[i].split("")
+    new_array.push("!")
+    new_array[0]=new_array.first.upcase
+    array[i]=new_array.join
+    i +=1
+  end
+  return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+
+  array.any? do |call|
+    new_array=call.split("")
+    if new_array.length > 4
+      return true
+    end
+
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+i=0
+cheese_types = ["cheddar", "gouda", "camembert"]
+
+while i<3
+  if array.include?(cheese_types[i])
+    return cheese_types[i]
+  end
+  i +=1
+
+end
+return nil
 end
