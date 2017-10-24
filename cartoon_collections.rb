a1d2c8fce2343bf34e053b273a96429dc381be74
Min Kim <mk4555@nyu.edu>
2017-10-24 23:15:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require("pry")
+def roll_call_dwarves(s)# code an argument here
+  s.each_with_index { |dwarf, i| puts "#{i+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(s)# code an argument here
+  s.map!{|a| a.capitalize}
+  s.map{|a| a += "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(s)# code an argument here
+  s.any? {|a| a.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(s)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ans = true
+  s.each do |a|
+    if cheese_types.include?(a)
+      return a
+    else
+      ans = nil
+    end
+  end
+  ans
 end
