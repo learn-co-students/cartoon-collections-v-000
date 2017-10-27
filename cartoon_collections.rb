306ad7ee8f161e77cd2b8f64ba9a3beab872dd73
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 18:57:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|name, index|}
+   puts "#{index+1}. {name}"
+
 end
 
 def summon_captain_planet# code an argument here
