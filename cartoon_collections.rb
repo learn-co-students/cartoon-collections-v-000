ff9741b0a6dfe12b82f5d8d511e7a31aa9aa74e3
Morgan Fudge <mfudge92@gmail.com>
2017-11-01 12:31:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each do |name|
+    puts name
+  end
 end
 
 def summon_captain_planet# code an argument here
