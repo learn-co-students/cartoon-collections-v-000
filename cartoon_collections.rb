b4eafcd20d029ef75098ce9a10198d7562c820c7
Cat Norris <cannenorris@yahoo.com>
2017-12-05 23:37:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+  puts "#{index}.  #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(planateer_calls)
+  planateer_calls.map { |element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(calls)
+calls.each do |word|
+  return true if word.length > 4
+  end
+  false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
+#find executes the block you provide for each element in the array.
+#If the last expression in the block evaluates to true, the find method
+#returns the value and stops iterating. If it doesn't find anything after iterating through all of the elements, it returns nil.
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  snacks.find {|fooditem| cheese_types.include?(fooditem)}
+  end
