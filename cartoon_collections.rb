b19e3df2926252efb0811871684e710c1b08378c
jtorre20 <jtorres1989@gmail.com>
2017-12-11 16:13:16 -0500

    refactored added include

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,16 @@ def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find do |cheese| 
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    #cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    cheese_types.include?(cheese)  
   end
 end
+
+=begin
+def find_the_cheese(food)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if food.include?(cheese_types)
+    return cheese_types
+  end
+end
+=end
