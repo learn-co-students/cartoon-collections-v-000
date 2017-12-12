be8e7f885fbe3ef0f9460509c30a48c82497b848
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-12 13:51:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
+require "pry"
 def roll_call_dwarves(dwarves)
-  # Your code here
+  dwarves.each_with_index do |name,index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  my_fruits = []
+  planeteer_calls.each do |fruits|
+    my_fruits << "#{fruits.capitalize}!"
+  end
+  my_fruits
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  if calls_long.length > 4
+    true
+  else calls_long.length < 4
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find { |cheese| cheese == "cheddar" }
+  #binding.pry
 end
