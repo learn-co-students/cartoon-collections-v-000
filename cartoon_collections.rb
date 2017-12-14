9bb306070dc12aa90874e461a0d5c877a9b6cbe8
rh24 <ms.rebeccahong@gmail.com>
2017-12-13 20:42:54 -0500

    add punctuation

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |dwarf, index| puts "#{index+1} #{dwarf}" }
+  dwarves.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
 end
 
 def summon_captain_planet(planeteer_calls)
