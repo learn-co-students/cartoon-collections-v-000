bd42e79877be6766e61e7ac496708695820093d1
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 21:13:31 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,20 +34,3 @@ def find_the_cheese(word)
     end
   end
 end
-
-
-
-# def my_all?(collection)
-#   i = 0
-#    block_return_values = []
-#    while i < collection.length
-#      block_return_values << yield(collection[i])
-#      i += 1
-#    end
-#
-#    if block_return_values.include?(false)
-#      false
-#    else
-#      true
-#    end
-# end
