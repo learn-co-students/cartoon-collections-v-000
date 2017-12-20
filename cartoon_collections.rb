689109962d232fea4fe4d3518cf649b9bdf3d750
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 13:14:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,17 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
+#formal solution...DO NOT UNDERSTAND!!
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+
+#my solution is very tedious
+=begin
 def find_the_cheese(recipe)
   #cheeses = ["cheddar", "gouda", "camembert"]
   #"cheddar" || "gouda" || "camembert"
@@ -29,3 +40,5 @@ def find_the_cheese(recipe)
       nil
     end
   end
+
+=end
