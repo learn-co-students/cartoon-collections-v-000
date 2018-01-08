1947055c49fce141e7d81ae69a688e32440973cf
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-08 03:08:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |calls| calls.capitalize + "!"  }
 end
 
 def long_planeteer_calls# code an argument here
