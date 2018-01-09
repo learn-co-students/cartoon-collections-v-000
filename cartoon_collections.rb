6c4ae0ba6f3f418160f4bcc48161b165c44faf06
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-09 03:38:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@
 dwarves  = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_array  = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 short_words = ["puff", "go", "two"]
-ingredients = ["garlic", "cheddar", "rosemary", "gouda", "bread"]
-
+ingredients = ["garlic", "cheddan", "rosemary", "gouda", "bread"]
+array = []
 
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
@@ -13,37 +13,30 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_array)
-  array_returned = planeteer_array.collect do |planeteer|
+  planeteer_array.collect do |planeteer|
     "#{planeteer.capitalize}!"
   end
-
 end
-summon_captain_planet(planeteer_array)
-
 
 def long_planeteer_calls(array)
   element_len4 = false
   array.each do |element|
     if element.length > 4
-      element_len4 = true
+      return true
     end
   end
-  element_len4
+  return false
 end
 
 def find_the_cheese(ingredients)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_found = nil
-  cheese_found =  ingredients.collect do |element| if cheese_types.include?(element) = true
-                                                      puts "fffffffffffff"
-                                                     end
-
-  end
-  puts "cheese_found #{cheese_found}"
-
+  ingredients.collect  do |element|
+                          if cheese_types.include?(element)
+                             return cheese_found = element
+                          end
+                       end
+  cheese_found
 end
-find_the_cheese(ingredients)
-
 
 #!/usr/bin/env ruby -wKU
