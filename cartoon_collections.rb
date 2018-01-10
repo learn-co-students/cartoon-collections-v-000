4e80eb90c838ef7adb04ac8d477246d06214b11b
Kriti Rai <kriti.rai@gmail.com>
2018-01-10 18:45:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,34 +3,15 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  string = array.join("! ")
-  string <<"!"
-  new_array = string.split(" ")
-  new_array.map {|i| i.capitalize}
+  array.map {|i| i.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
-  i = 0
-	while i < array.length
-		if array[i].length > 4
-			return true
-		else
-			return false
-		end
-		i +=1
-	end
+  array.any? {|i| i.length>4}
 end
+
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  counter = 0
-  while counter < array.length
-	  array.each do |i|
-	  	if cheese_types.include?(i)
-	  		return i
-	  	end
-	  	counter +=1
-	  	end
-	  	return nil
-	  end
+  array.find {|i| cheese_types.include?(i)}
 end
