dbd14502d730e165e70532619c548d703a87001c
jacobsilver2 <jacobsilver2@mac.com>
2018-01-11 16:05:38 -0500

    did the work

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(arr)
+   arr.each_with_index do |name, i|
+    puts "#{i+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map do |w|
+    letters = w.split('')
+    letters.first.upcase!
+    w = letters.join
+    w << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any?{|w|
+    w.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.detect{ |c|
+    cheese_types.any? { |cheese|
+      c.include?(cheese)
+    }
+  }
 end
