0ed771ec47126d6519eaebf6f2d12d00061a3ea4
Matthew Crooks <mfcrooks@gmail.com>
2018-01-12 06:32:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+test = ["foo", "bar", "test", "strings"]
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf,index|
+    puts "#{index}. #{dwarf}"
+  end
+end
+roll_call_dwarves(test)
+def summon_captain_planet(planet_calls)
+  planet_calls.map { |call| call.capitalize + "!" }
+end
+summon_captain_planet(test)
+def long_planeteer_calls(planet_calls)
+  planet_calls.any? { |call| call.length > 4 } #returns true if any words are bigger than 4 chars
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(could_be_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  could_be_cheese.find do |possible_cheese|
+      cheese_types.include?(possible_cheese)
+  end
 end
