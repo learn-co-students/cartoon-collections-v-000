270eeea7f762d928cf1ac8cc03ee364ee17a0ec9
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 09:38:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index.map { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.map { |veg| "#{veg.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese# code an argument here
