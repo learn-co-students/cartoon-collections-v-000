68d698742f7e15ee5e736a9a1a25679c0ada3322
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 22:09:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,10 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
+  calls_long.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find { |food| cheese_types.include? food }
 end
