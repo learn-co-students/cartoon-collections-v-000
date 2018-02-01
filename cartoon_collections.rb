1a483b1830dc964208e6e3f49729f1fd197ad246
Aderianna T Williams <aderiannaw@yahoo.com>
2018-02-01 00:58:45 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,14 +8,29 @@ end
 def summon_captain_planet(planeteer)# code an argument here
   planeteer_calls = []
 planeteer.collect! do |planeteers|
+  planeteer
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(four) # code an argument here
+ i = 0 
+ while i < four.length
+ return four[i] if yield(four[i])
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0 
+  while < cheese.length
+  cheese_types << yield(cheese[i])
+  i = i + 1
 end
+if cheese_types.include?(cheese)
+   cheese
+ else
+   nil
+ end
+ end
+  
