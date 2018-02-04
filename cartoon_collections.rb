03d88ce1ed43fcb49db45832d3cf67e0d9846b86
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-04 20:53:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,16 @@
-def roll_call_dwarves(names_array)
-  names_array.each_with_index do |name, index|
-    puts "#{index+1}. #{name}"
-  end
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index| puts "#{index + 1} #{dwarf} /n"}
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
-    "#{call.capitalize}!"
-  end
+  planeteer_calls.collect {|planeteer_call| planeteer_call.capitalize << "!"}
 end
 
-def long_planeteer_calls(words)
-  words.any? do |word|
-    word.length > 4
-  end
+def long_planeteer_calls(calls_array)
+  calls_array.any? {|call| call.size > 4}
 end
 
-def find_the_cheese(foods)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    cheese_types.find do |cheese|
-      foods.include?(cheese)
-  end
+def find_the_cheese(strings_array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  strings_array.find {|string| cheese_types.include?(string)}
 end
