a8468a3c31fb56875102d57d5419796bd126edc0
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 03:38:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each do |dwarf|
+    put "#{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
