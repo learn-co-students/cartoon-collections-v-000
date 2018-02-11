1e6d2d67941f5a66bfe24f77d6b732b9feb5a019
Eva Smidt <shevangeline@gmail.com>
2018-02-11 00:49:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+    planeteer_calls.any?  { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+#The find_the_cheese method should accept an array of strings.
+#It should then look through these strings to find and return the first string
+#that is a type of cheese.
+#The types of cheese that appear are "cheddar", "gouda", and "camembert".
+def find_the_cheese(snacks)
+  cheeses = %w(gouda cheddar camembert)
+  snacks.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
