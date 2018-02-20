564a6c095f14838271ff6a393c6bf4feb193668e
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 21:01:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,12 +5,11 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! {|call| "#{call.capitalize}!"}
-  planeteer_calls
+  planeteer_calls.map {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.detect {|call| call.length > 4} ? true : false
+  planeteer_calls.any? {|call| call.length > 4} 
 end
 
 def find_the_cheese(snacks)
