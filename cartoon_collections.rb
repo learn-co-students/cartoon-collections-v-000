7c6428dee84cd36ab6ce690c0952c4e967294b9e
Reno Sorensen <renojames19@gmail.com>
2018-02-22 03:10:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  numbered_dwarves = []
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i +1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
