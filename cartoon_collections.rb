829b14768321c8eb19c49d2f1fe4f6746b3f7204
Reno Sorensen <renojames19@gmail.com>
2018-02-23 02:39:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
