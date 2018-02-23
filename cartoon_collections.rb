2bda974fc6e4d842224a9ccf319c0e424abecd0e
Reno Sorensen <renojames19@gmail.com>
2018-02-23 02:25:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |element|
+    "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
