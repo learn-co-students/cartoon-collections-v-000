a13691f9485e2d470f916ebf7bcff4f1a6e1570c
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 02:22:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
+end
+#
+# WORKING WITHOUT INCLUDE
+def find_the_cheese(yummies)
+  yummies.find{|ingr| ingr == "cheddar" || ingr == "gouda" || ingr == "camembert"}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+#WORKING WITH INCLUDE IN TERMINAL BUT NOT PASSING TESTS
+# def find_the_cheese(recipe) 
+#   cheese_types = ["cheddar", "gouda", "camembert"] 
+#   recipe.each do |ingr|
+#     if cheese_types.include?(ingr)
+#      return ingr
+#     else
+#      puts "No cigar"
+#     end
+#   end
+# end
