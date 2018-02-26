897253700b4c2263f08e9cabd12c9c1e2d1feb0e
Christine Cua <cloudycua@gmail.com>
2018-02-26 17:57:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each.map { |planeteer| "#{planeteer.capitalize}!" }
+  planeteer_calls.map { |planeteer| "#{planeteer.capitalize}!" }
 end
 
 def long_planeteer_calls(call_array)
