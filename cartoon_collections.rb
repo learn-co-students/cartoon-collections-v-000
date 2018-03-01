bda0a154e8161f50aedd79a4981d5d7d5eb09fa0
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-01 23:33:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+    dwarves.each_with_index { |item, index|
+   dwarves[item] = index
+    }
+    dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet()
+ 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
