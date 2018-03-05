1cac324a9d5293edbd1489a0c005ef957f4ec81f
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-05 07:55:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect {|element| element.capitalize}
+  i = 0
+  array.collect {|element| element[i].capitalize.join("!")  i+= 1}
 end
 
 def long_planeteer_calls# code an argument here
