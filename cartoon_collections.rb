8a73cfe70e2ea8ff1ae278681a13083ca293408a
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-05 07:53:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+
+  array.each_with_index {|name,index| puts "#{index+=1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect {|element| element.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
