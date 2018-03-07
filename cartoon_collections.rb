55942ca81d9c0d21a32232776ddabfe7c4ce0464
Victoria Meng <nights1ststar@protonmail.com>
2018-03-07 22:38:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,12 @@ def summon_captain_planet(calls)
   calls.map { |call| "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  items.find { |item| cheese_types.include?(item) }
 end
