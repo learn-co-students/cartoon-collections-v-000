bc16a97dd96ad99b351e742756be191121671168
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-09 18:54:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    number = index + 1 
+    puts "#{number}. #{name}/n"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |call|
+    call.capitalize! << "!"
+  end
+return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  if array.length < 4 
+    false 
+  else 
+    true
+ end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    if cheese_types.include?(cheese)
+      return cheese 
+    else nil 
+  end
+end
 end
