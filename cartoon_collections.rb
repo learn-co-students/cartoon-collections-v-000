586d1ffd7b48844f3e59e894cbb97f150943a449
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 01:00:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index_number|
+  puts "#{index_number + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    end
+    planeteer_calls.capitalize
+    planeteer_calls.join(""",")
+  puts "#{call[0]}! #{call[1]}! #{call[2]}! #{call[3]}!"
+  summon_captain_planet
 end
 
 def long_planeteer_calls# code an argument here
