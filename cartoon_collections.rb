eff45b1ae9b10761c330f9d4f39c6854892c7dd6
Michael Blandford <mdblandford@gmail.com>
2018-04-19 14:44:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index {|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
+  # .each_with_index calls 2 arguments and enumerates each item
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect{|call| call.capitalize + "!"}
 end
+  # .collect OR .map creates a new array containing the values returned by the block
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |x|
+    x.length > 4
+  end
 end
+  # .any? allows us to search an array for elements containing the desired charcacterisitc
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foodArray)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foodArray.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
+  # proper syntax for an .include? method
