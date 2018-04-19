f26f1bd4402c17b5bcd470ad8579d89015a8aea1
valeria campilongo <cvaleriac@hotmail.it>
2018-04-19 21:40:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves = ["Dopey", "Grumpy", "Bashful",]# Your code here
+ dwarves.each_with_index do |dwarf, index|
+   puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet (veggies)#
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |veggie|
+    veggie.capitalize + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  long_planeteer_calls.each do |call|
+   if call.length > 4
+  return true
+else
+  return false
+    end
+   end
 end
 
 def find_the_cheese# code an argument here
