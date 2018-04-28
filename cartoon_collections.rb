dc45e7c6e1f18b7c73cb026fa7cfb747c072a9cc
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-28 16:13:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)# code an argument here
+  dwarfs.each_with_index do |names, index|
+    puts "#{index + 1}. #{names}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.map do |planet|
+    "#{planet.capitalize}!"
+  end# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_type)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_type.detect do |cheese|
+    cheese.include?("cheddar")
+  end
 end
