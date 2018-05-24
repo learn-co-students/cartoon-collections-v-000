0739e270e1f7221795fbe9cef2dd277a5bc5f5a8
Mohammed Noori <kmanoorib@gmail.com>
2018-05-24 23:58:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+
+  count = 1
+  array.each do |name|
+    puts "#{count}. #{name}"
+    count+=1
+  end
+
+  #commented code works the same as below...well it should(it doesn't)
+=begin
+  array.each_with_index |name, index| {
+    puts "#{index+1}. #{name}"
+  }
+=end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  array.collect {|name| name.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  greater_4 = false
+  array.each do |name|
+    if name.length > 4
+      greater_4 = true
+    end
+  end
+  greater_4
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    cheese_types.include?(food)
+  end
 end
