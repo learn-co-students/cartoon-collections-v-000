a9e20521777714c1492dc7c6f34ec0f89cf9410e
Leticia Molina <letimolina@gmail.com>
2018-05-26 20:15:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,11 @@ def summon_captain_planet(calls)
   calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  call.any? { |phrase| phrase.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find { |cheese| cheese_types.include?(cheese) }
 end
