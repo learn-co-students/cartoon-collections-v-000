02cc20161117362a15ac710aae40a54a1aed2680
Leticia Molina <letimolina@gmail.com>
2018-05-26 19:58:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,14 +2,14 @@ def roll_call_dwarves(names)
   count = 1
   i = 0
   while count < names.length+1
-  puts "#{count}. #{names[i]}"
-  count +=1
-  i +=1
-end
+    puts "#{count}. #{names[i]}"
+    count +=1
+    i +=1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
