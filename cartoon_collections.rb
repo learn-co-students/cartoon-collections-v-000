c0a2f5590f458c02f29a7f4059faa2e1597a584e
Marie Cain <mariecain27@gmail.com>
2018-06-05 22:13:36 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,52 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+#  counter = 0 
+#  while counter < array.length 
+#    puts "#{counter + 1}. #{array[counter]}"
+#    counter += 1 
+#  end
+  
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#roll_call_dwarves(array)
+  
+def summon_captain_planet(array)
+#  new_array = []
+#  counter = 0 
+#  while counter < array.length 
+#    new_array << array[counter].capitalize + "!"
+#    counter += 1 
+#  end 
+#  new_array
+  array.collect do |earth_elements|
+    earth_elements.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#summon_captain_planet(array)
+
+def long_planeteer_calls(array)
+  longer_than_four = false
+  array.each do |words|
+    if words.length > 4
+      longer_than_four = true
+    else
+      longer_than_four = false 
+    end
+  end
+    longer_than_four
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#long_planeteer_calls(array)
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |i|
+    cheese_types.include?(i)
+  end
 end
+
+#find_the_cheese(array)
