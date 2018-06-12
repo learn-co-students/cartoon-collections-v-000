b52f55ba3d0c7a12e5f40a65c5eae470926e44f2
Wendy Calderon <wendycalderon@gmail.com>
2018-06-12 03:17:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  print_out = []
+  dwarves.each_with_index do |name, index|
+    print_out << "#{index +=1} #{name}"
+  end
+  puts print_out
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet (planeteer_calls)
+  calls_collection = []
+  testtt = planeteer_calls.map! {|calls| calls.capitalize}
+  testing = testing.join(!)
+  testing
+  
 end
+
+
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
