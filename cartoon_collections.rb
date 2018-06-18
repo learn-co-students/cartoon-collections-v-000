e6445312710d69b7c3749da2966a1e1891c93087
parisdolfman <32081304+parisdolfman@users.noreply.github.com>
2018-06-18 13:35:14 -0400

    Add notes

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,19 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do | name , index |
-    puts "#{index + 1} #{name}"
+def roll_call_dwarves(dwarves) #takes in argument 'dwarves' which is suplied by user
+  dwarves.each_with_index do | name , index | #calling .each_with_index on the argument and for each round assigns the name as user input and index as number in array
+    puts "#{index + 1} #{name}" #interpolating index to produce number and add 1 to make sense to user, plus name
   end
 end
 
 def summon_captain_planet(call)
-  call.collect do |call|
-  call.capitalize + "!"
+  call.collect do |call| #calling .collect runs the argument through the block and produces a new array 
+  call.capitalize + "!" #new array is changed to according to whats written here 
  end
 end
 
 def long_planeteer_calls(calls)
-  answer = false
-  calls.each do |call|
-    if call.length > 4
+  answer = false #the answer is set to the base false to bechanged depending on conditions below
+  calls.each do |call| #we are rotating through each argument supplied by user and naming each argument the variable 'call'
+    if call.length > 4 #if the length of the variable call is less than four letters long the answer is now true as per below
       answer = true
     end
   end
@@ -21,9 +21,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(delicious_cheeses)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  delicious_cheeses.find do |cheese|
+  cheese_types = ["cheddar", "gouda", "camembert"] #array
+  delicious_cheeses.find do |cheese| #we are selecting the elements of the array with .find and naming each variable 'cheese'
     cheese_types.include?(cheese)
   end
 end
