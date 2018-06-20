8272c0ee86453dda6c18585507208353384d27f6
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:10:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index {|i, e| puts "#{e+1} #{i}"}
 end
 
 def summon_captain_planet# code an argument here
