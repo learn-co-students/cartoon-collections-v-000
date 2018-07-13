fdf3f65e25361cb6f4fc8183cc4fe9524376bc2c
Stacy Carr <stacy@boostersinabox.com>
2018-07-13 19:28:31 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index| puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! { |calls| calls.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+
+def long_planeteer_calls(array)
+  if array.each.size > 4
+  true
+else
+  false
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+ingredients = ["garlic", "rosemary", "bread"]
+
+
+def find_the_cheese(array)
+  cheeses = ["cheddar", "gouda", "camebert"]
+  array.find do |item|
+    cheeses.include?(item)
 end
+end
+  
