3d5d0db118adf08dff549432aa29045907fc6942
Dana Palmer <roiai4@hotmail.com>
2018-07-16 06:43:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  roll_call = []
+  array.each_with_index {|name, index| puts "roll_call << #{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  summon = []
+  calls.collect { |element| summon << element.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(longer_than_4)
+  longer_than_4 { |word| }
+  if word.length > 4
+  puts "longer than 4"
+else
+  false 
+end
 end
 
 def find_the_cheese# code an argument here
