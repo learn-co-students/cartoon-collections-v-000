26eb6a1cb7be12a3584b269eb7ec6b61c53a3394
Anna Dybas <a.m.dybas@gmail.com>
2018-07-17 04:37:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,14 +5,20 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map { |food| food.capitalize }
+  new_array = []
+  array.map { |food| new_array << food.capitalize.to_s + "!" }
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
