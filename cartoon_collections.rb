99a23a6b6b0fc22fe405211c497b1e141696c7ea
Juan Tenezaca <tenj34@gmail.com>
2018-07-21 18:18:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  i = 0
+  while i < dwarves.length
+    puts "#{i+1}. #{dwarves[i]}"
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer)# code an argument here
+  #need to understand more on how collect do works
   # Your code here
+  update_planeteer = [] #to store our new planeteer
+  planeteer.collect do |citizen|
+    update_planeteer<< "#{citizen.capitalize}!"
+  end
+  update_planeteer.to_a
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? { |i| i.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  snacks.find{|snack| cheese_types.include?(snack)} 
+
 end
