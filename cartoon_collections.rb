c92dfc75948801e749baa73e5105e681b8acb988
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-23 02:12:53 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,46 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = %w(Doc, Dopey, Bashful, Grumpy)
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|item, index| puts "#{index+1} #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "heart"]
+
+def summon_captain_planet(planateer_calls)
+  #=> capitalize and add exclamation point
+  planateer_calls.map {|element| "#{element.capitalize}" + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+words = ["puff", "go", "two", "industrious", "bop"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def long_planeteer_calls(words)
+  words.any? do |word| 
+    word.length > 4
+  end
+end
+  
+#   if block_given?
+#     block_return_values = []
+#     i = 0
+    
+#   while i < words.length
+#     block_return_values << yield(words[i])
+#     i += 1
+#   end
+# end
+# end
+
+# long_planeteer_calls(["hel", "hi"]) do |item| 
+#   if item.length > 4
+#     puts true
+#   end
+# end
+
+array = []
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |el|
+  el == "cheddar" || el == "gouda" || el == "camembert"
+end
 end
