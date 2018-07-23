cf128a2723aa7d17f4be20fe4eb5d1b2f2a6180a
Mika88 <mikaspivak@gmail.com>
2018-07-23 17:51:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,20 +12,13 @@ end
 
 def long_planeteer_calls(array)
   array.any? do |word|
-    if word.length > 4
-      true
-    else
-      false
-    end
+    word.length > 4
   end
 end
 
 def find_the_cheese(array)
   array.find do |word|
-    if word == "cheddar" || word == "gouda" || word == "camembert"
-      true
-    else
-      false
-    end
+    cheese = ["cheddar", "gouda", "camembert"]
+    cheese.include?(word)
   end
 end
