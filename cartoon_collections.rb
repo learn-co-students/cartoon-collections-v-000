e932a0c15894c07a5471afa55cf8e17853b33915
J Fern <jfern20@gmail.com>
2018-08-14 04:50:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   
-  planeteer_calls.map { |powers_combined| puts powers_combined.upcase + "!"}
+  planeteer_calls.map { |powers_combined| 
+  powers_combined.capitalize +  "!"}
   
 end     
 
