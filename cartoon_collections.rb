5fec8139c7310b8af3c047f99c6b9948db796e67
taisaoko <ttd3calvin@gmail.com>
2018-08-25 16:15:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 
-def roll_call_dwarves(dwarves) # code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
-  dwarves.each.with_index(1) do|dwarf, index| 
-  puts "#{index}.#{dwarf}"
+  array.each_with_index do|dwarf, index| 
+  puts "#{index + 1}.#{dwarf}"
   end
 end
 
