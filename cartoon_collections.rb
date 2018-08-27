468a2b81ea16f3fccb3d6815229ccda1ab089219
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-08-27 02:39:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,8 @@ calls.any? {|chant| chant.length > 4}
 end
 
 def find_the_cheese(cheese_type)
-  cheese_type.find do |cheese|
-    if "#{cheese}" == "cheddar" || "#{cheese}" == "gouda"
-"#{cheese}"
-end
+cheeses = ["gouda", "cheddar", "camembert"]
+cheese_type.find do |cheese|
+cheeses.include?(cheese)
 end
 end
