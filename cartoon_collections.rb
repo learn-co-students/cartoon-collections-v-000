fe005fe6382169a21fd53bb91bdfec924b6cf02f
Allison Howard <alliemhow@gmail.com>
2018-09-12 01:27:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index+1}#{dwarf}"
-  end
+  dwarves.each_with_index { |dwarf, index| puts "#{index+1}#{dwarf}" }
 end
 
 def summon_captain_planet(planeteer_calls)
