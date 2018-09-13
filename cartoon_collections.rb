39a05a1c6c81db9baaceb37e201bab235c3b4e28
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 06:10:54 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,23 +11,13 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-    array.select do |call|
-      if call.length > 4
-        return true
-      else
-        return false
-    end
-  end
+  array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese|
-    if array.include?(cheese)
-      return cheese
-    else
-      return nil
-    end
+  array.find do |cheese|
+    cheese_types.include?(cheese)
   end
 end
