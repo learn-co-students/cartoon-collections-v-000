252eee1f090353f98e90007a8961e3fed09742e5
Stef Claus <stef.claus@gmail.com>
2018-09-15 21:28:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.any? {|food| cheese_types.include?(food) }
-  
+  array.find_all {|food| cheese_types.include?(food) }.join(" ")
 end
+
