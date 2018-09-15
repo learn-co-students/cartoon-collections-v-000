8ee2766e772b77cdd66442fe3e9591aea11f4a4d
Stef Claus <stef.claus@gmail.com>
2018-09-15 21:37:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,7 @@ def long_planeteer_calls(array)
   array.any? {|i| i.length > 4}
 end
 
-
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find_all {|food| cheese_types.include?(food) }.join(" ")
 end
-
