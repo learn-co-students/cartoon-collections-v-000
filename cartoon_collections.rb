795f5d7dde47ea0f45e7f88585a5dcd31bb9da5c
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-15 02:38:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(list)# code an argument here
   # Your code here
+  list.each_with_index {|name, num| puts "#{num+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(list)# code an argument here
   # Your code here
+  list.collect {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(list)# code an argument here
   # Your code here
+  list.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.find {|ingredient| 
+    return ingredient if cheese_types.include?(ingredient)}
 end
