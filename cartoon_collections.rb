b9dcb01667e15fefb05b631a8369b2f0f205828a
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-22 01:31:37 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,3 +25,27 @@ def find_the_cheese(string)
     nil 
   end 
 end 
+
+
+
+# def roll_call_dwarves(dwarves)
+#   dwarves.each.with_index(1) do |dwarf, index|
+#     puts "#{index}. #{dwarf}"
+#   end
+# end
+
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map { |call| call.capitalize + "!" }
+# end
+
+# def long_planeteer_calls(planeteer_calls)
+#   planeteer_calls.any? { |call| call.length > 4 }
+# end
+
+# def find_the_cheese(potentially_cheesy_items)
+#   cheeses = %w(gouda cheddar camembert)
+
+#   potentially_cheesy_items.find do |maybe_cheese|
+#     cheeses.include?(maybe_cheese)
+#   end
+# end
