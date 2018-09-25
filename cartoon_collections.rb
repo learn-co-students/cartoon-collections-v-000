8d88851e89adade83440b4aa11426829d963f1e0
Paul Fell <swellpf@gmail.com>
2018-09-25 19:50:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) 
+  dwarves.each_with_index do |dwarf, index|
+  puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{ |element| element.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+   calls_long.each_with_index do |call,index|
+     if calls_long[index].length > 4
+       return TRUE
+      end
+    end
+    return FALSE
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    snacks.find { |snack| cheese_types.include?(snack) ? snack : nil}
 end
