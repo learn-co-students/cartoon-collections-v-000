536507d6d7468257414bda05efb2c4f7b5ca5282
Vince Andrew Pimentel <contact@vincep.me>
2018-10-14 22:05:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{|dwarf, num| puts "#{num + 1}: #{dwarf}" }
+  dwarves.each_with_index { |dwarf, num| puts "#{num + 1}: #{dwarf}" }
 end
 
 def summon_captain_planet(elements)
-  elements.collect{|element| "#{element.capitalize}!" }
+  elements.collect { |element| "#{element.capitalize}!" }
 end
 
 def long_planeteer_calls(calls)
-  calls.any?{ |call| call.length > 4 }
+  calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.find{|cheese| cheese_types.include?(cheese)}
+  cheeses.find { |cheese| cheese_types.include?(cheese) }
 end
