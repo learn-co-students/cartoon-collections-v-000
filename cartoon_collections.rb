97418691240181cc3e1fc078ce6602fff9a24414
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:26:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
 def roll_call_dwarves(list)
-  list.each {|curr| puts "#{curr}"}
+  count = 1
+  list.each do |curr|
+    puts "#{count}.*#{curr}"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  list.map do |curr|
+    curr.capitalize!
+    curr.concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  list.each do |curr|
+    if curr.length > 4
+      return true
+    end
+  end
+  false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.each do |curr|
+    if cheese_types.include?(curr)
+      return curr
+    end
+  end
+  nil
 end
