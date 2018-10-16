566ccd7c03af6cf0eb8a3bb5f5075ff937bf8875
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:02:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each {|curr| puts "#{curr}"}
 end
 
 def summon_captain_planet# code an argument here
