4df7ac64a4a061bcb27ef1ac087b49495eea7241
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-02 03:36:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(dwarves)
+  list = []
+  dwarves.each_with_index do |dwarf_name, index|
+    puts " #{index.to_i+1}. #{dwarf_name} "
+  end
+end
+  
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map{|e| e.capitalize<<"!"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+  if calls.any? {|x| x.length < 4}
+    true 
+  elsif calls.any? {|x| x.length >= 4}
+    false 
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.collect do |y|
+    if cheese_types.include?(y)
+      return y
+    end
+  end
+  nil #t should be nil because typically when looping over an array and not finding something, the return value is nil
 end
