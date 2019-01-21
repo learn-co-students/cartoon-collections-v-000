0fa2b8903b46dbd1964ff6564336431169bd47a6
Toni-Ty <antonique2479@yahoo.com>
2019-01-21 09:15:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(array)
+array.each_with_index do |value, index|
+  dwarves = "#{index.to_i+1}. #{value}"
+  end
+  puts "#{dwarves}"
+  end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+    array.map! { |n| n.capitalize + "!"}
+      returns "#{n}"
+  end
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+
 end
 
 def find_the_cheese# code an argument here
