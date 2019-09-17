9bbd258abbde38e8df750b5e04d8d6f4a507b5a9
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-09-17 19:17:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,9 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |i|
-    if cheese_types.include?(array[i])
-      array[i]
+  array.each do |item|
+    if cheese_types.include?(array[item])
+      array[item]
     else
       nil
     end
