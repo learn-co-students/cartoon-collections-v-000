1cce6c740b0e7f68bc119e1babd25dc1e812956e
Tom Sharkey <tbsharkey@gmail.com>
2020-03-01 08:32:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,14 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|item, index|
-    puts "#{index + 1}. #{item}" }
+  dwarves.each_with_index {|dwarf, index|
+    puts "#{index + 1}. #{dwarf}" }
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls.collect{ |i| i.capitalize + "!" }
+  planeteer_calls.collect{ |i| i.capitalize + "!" }
 end
 
 def long_planeteer_calls(calls)
-calls.any? {|i| i.length > 4}
+  calls.any? {|i| i.length > 4}
 end
 
 def find_the_cheese(snacks)
