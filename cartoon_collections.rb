530624cafeb6666feb8ee58360dae7d4d082c1fd
Vicky Theresa Tak <vttak9@gmail.com>
2020-04-02 00:17:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarves)# code an argument here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+dawrves.each_with_index { |item, index|
+  i = 0
+    while item < index.length
+      { |name| puts "#{i+1}. #{name}" }
+      i += 1
+    end
+}
+
+end
+
+
+roll_call-dwarves(dwarves)
+end
 end
 
 def summon_captain_planet# code an argument here
