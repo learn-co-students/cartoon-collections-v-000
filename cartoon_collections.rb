92a3d08ab84a13146adc73422eadde7c6652b95f
Vicky  Tee <vttak9@gmail.com>
2021-01-13 23:40:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,31 +1,56 @@
+require 'pry'
 
 def roll_call_dwarves(dwarves)# code an argument here
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-dawrves.each_with_index { |item, index|
-  i = 0
-    while item < index.length
-      { |name| puts "#{i+1}. #{name}" }
-      i += 1
-    end
+hash = Hash.new
+dwarves.each_with_index { |index, item|
+  hash[index] = item
+  binding.pry
 }
+puts hash
+
 
 end
 
 
-roll_call-dwarves(dwarves)
+def kesha_maker(array)
+
+  newarray = Array.new
+  array.each {
+    |name|
+
+name[2] = "$"
+newarray << name
+}
+return newarray
+end
+
+
+def summon_captain_planet(array)# code an argument here
+
+newarray = Array.new
+array.each {
+  |name|
+name.capitalize!
+name[name.length] = "!"
+
+newarray << name
+}
+return newarray
+end
+
+
+def long_planeteer_calls(array)
+if array.any? {|element| element.length > 4}
+  return true
+else
+  return false
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def find_the_cheese(array)
+  cheeses = ["gouda", "cheddar", "camembert"]
+  array.find {|food| cheeses.include?(food)}
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
